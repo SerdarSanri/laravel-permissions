89fd6bfbb356e877ef1816d9a2af11810de607ea
Jaggy Gauran <jaggygauran@gmail.com>
2014-09-04 16:50:53 +0800

    make fields the bare minimum

--- src/migrations/2014_09_04_074512_create_roles_table.php
+++ src/migrations/2014_09_04_074512_create_roles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRolesTable extends Migration
 {
 
     /* public up() {{{ */
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function(Blueprint $table)
+        Schema::create('roles', function(Blueprint $table)
         {
             $table->increments('id');
 
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name');
+            $table->string('description');
 
             $table->timestamps();
         });
@@ -37,7 +37,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('roles');
     }
     /* }}} */
 }
